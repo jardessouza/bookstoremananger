ec0bf439a00700066506bba7d9ccad086a651f91
Jardes Andre <jardessouza@gmail.com>
2022-12-20 11:28:24 -0300

    Developed author by id functionality

---
+++ src/main/java/com/jardessouza/bookstoremanager/model/authorentity/exception/AuthorNotFoundException.java
@@ -0,0 +1,9 @@
+package com.jardessouza.bookstoremanager.model.authorentity.exception;
+
+import javax.persistence.EntityNotFoundException;
+
+public class AuthorNotFoundException extends EntityNotFoundException {
+    public AuthorNotFoundException(Long id) {
+        super(String.format("Author with id %s not exists", id));
+    }
+}
--- src/test/java/com/jardessouza/bookstoremanager/author/controller/AuthorControllerTest.java
+++ src/test/java/com/jardessouza/bookstoremanager/author/controller/AuthorControllerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.web.PageableArgumentResolver;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
